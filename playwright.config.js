5bb4b24d4aa3734a69e3d0c775ae9dae932bbccd
nikhiljsupekar <nikhiljsupekar@gmail.com>
2026-03-18 23:26:52 +0530

    Added trace and video recording in Playwright config

--- playwright.config.js
+++ playwright.config.js
@@ -30,7 +30,13 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    /*trace: 'on-first-retry',
+    video: 'retain-on-failure',
+    screenshot: 'only-on-failure',*/
+    trace: 'on',          // record trace for ALL tests
+    video: 'on',          // record video for ALL tests
+    screenshot: 'on',     // optional (captures screenshots)
+    headless: false,
   },
 
   /* Configure projects for major browsers */
